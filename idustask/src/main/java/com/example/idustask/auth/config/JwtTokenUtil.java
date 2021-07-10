935c1ae1e56caecbcf011d5774affd7306475816
= <wjdrlans4000@naver.com>
2021-07-10 16:46:53 +0900

    refactor : yaml 변경 및 @ConfigurationProperteis 사용
    Properties -> yaml 로 변경
    @PropertySource -> @ConfigurationProperteis 사용
    Spring Boot 자동 설정 사이클과 맞지 않아서 설정이 제대로 안될수 있으므로 권장하지 않음
    jwt_token_validity도 주입받도록 설정

--- idustask/src/main/java/com/example/idustask/auth/config/JwtTokenUtil.java
+++ idustask/src/main/java/com/example/idustask/auth/config/JwtTokenUtil.java
@@ -3,7 +3,10 @@ package com.example.idustask.auth.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -14,14 +17,15 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Component
-@PropertySource("classpath:application.properties")
+@ConfigurationProperties("jwt")
+@Getter
+@Setter
 public class JwtTokenUtil {
     // 실제로 signWith에 해당하는 부분은 훨씬더 복잡한 Key를 byte형식으로 생성해서 줘야된다.
     // 원래 이 secret 키는 외부 노출되면 안됨
-    @Value("${jwt.secret}")
     private String secret;
 
-    public static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60;
+    private long jwt_token_validity;
 
     //토큰으로 유저네임 확인
     public String getUsernameFromToken(String token) {
@@ -60,7 +64,7 @@ public class JwtTokenUtil {
                 .setClaims(claims) //Payload(내용) 정보 하나하나를 각각 클레임 (claim)이라고 부른다 Claim은 key/value
                 .setId(id)
                 .setIssuedAt(new Date(System.currentTimeMillis())) //토큰발행된 시간
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 100)) //토큰 만료시간 설정 1시간
+                .setExpiration(new Date(System.currentTimeMillis() + jwt_token_validity * 100)) //토큰 만료시간 설정 1시간
                 .signWith(SignatureAlgorithm.HS512, secret) //토큰을 탈취하지 못하도록 secret key로 Hash값을 추출해 비밀키로 복호화한 뒤 토큰의 뒤에 붙여줍니다.
                 .compact(); //토큰생성
     }
